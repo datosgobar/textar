94084b9e9cf882c90fe76d834a85b74f0d3d539f
Agustín Benassi <agusbenassi@gmail.com>
2016-11-25 08:44:45 -0300

    Comento el setup de los tests por ahora.

--- tests/test_text_classifier.py
+++ tests/test_text_classifier.py
@@ -15,31 +15,33 @@ class TestTextClassifier(unittest.TestCase):
 
     def setUp(self):
         """Carga de los datos de prueba (20 Newsgroups corpus)."""
-        TEST_DIR = "/home/mec/testData/textos/20_newsgroups/"
-        cats = os.listdir(TEST_DIR)
-        cats_totales = []
-        contenidos = []
-        for cat in cats:
-            temp_list = os.listdir(os.path.join(TEST_DIR, cat))
-            cats_totales = cats_totales + [cat for i in range(len(temp_list))]
-            temp_list = map(lambda x: os.path.join(TEST_DIR, cat, x),
-                            temp_list)
-            for filename in temp_list:
-                with codecs.open(
-                        filename, encoding='latin1', mode='r') as content_file:
-                    lines = content_file.readlines()
-                    filtered_lines = [line for line in lines[11:] if line[0] not in ['$', '>']]
-                    content = ''.join(filtered_lines)
-                contenidos.append(content)
+        # TEST_DIR = "/home/mec/testData/textos/20_newsgroups/"
+        # cats = os.listdir(TEST_DIR)
+        # cats_totales = []
+        # contenidos = []
+        # for cat in cats:
+        #     temp_list = os.listdir(os.path.join(TEST_DIR, cat))
+        #     cats_totales = cats_totales + [cat for i in range(len(temp_list))]
+        #     temp_list = map(lambda x: os.path.join(TEST_DIR, cat, x),
+        #                     temp_list)
+        #     for filename in temp_list:
+        #         with codecs.open(
+        #                 filename, encoding='latin1', mode='r') as content_file:
+        #             lines = content_file.readlines()
+        #             filtered_lines = [line for line in lines[11:] if line[0] not in ['$', '>']]
+        #             content = ''.join(filtered_lines)
+        #         contenidos.append(content)
+
         # sacar los textos muy cortos
-        largo = map(len, contenidos)
-        contenidos = [c for c, l in zip(contenidos, largo) if l > 100]
-        labels = cats_totales
-        labels = [lab for lab, l in zip(cats_totales, largo) if l > 100]
-        self.ids = [str(i) for i in range(len(contenidos))]
-        self.texts = contenidos
-        self.labels = labels
-        self.tc = TextClassifier(self.texts, self.ids, encoding='latin1')
+        # largo = map(len, contenidos)
+        # contenidos = [c for c, l in zip(contenidos, largo) if l > 100]
+        # labels = cats_totales
+        # labels = [lab for lab, l in zip(cats_totales, largo) if l > 100]
+        # self.ids = [str(i) for i in range(len(contenidos))]
+        # self.texts = contenidos
+        # self.labels = labels
+        # self.tc = TextClassifier(self.texts, self.ids, encoding='latin1')
+        pass
 
     def test_reload_texts(self):
         # TODO aca van los tests!
